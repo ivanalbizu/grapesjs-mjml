77fb13bdebfda7e3b473408f2748470e3048e80a
ivanalbizu <mariano-ivan.gonzalez-albizu@capgemini.com>
2022-12-19 21:47:10 +0100

    set default values components

--- src/components/Divider.ts
+++ src/components/Divider.ts
@@ -22,8 +22,8 @@ export default (editor: grapesjs.Editor, { coreMjmlModel, coreMjmlView }: any) =
           'border-color': '#000000',
           'padding-top': '10px',
           'padding-bottom': '10px',
-          'padding-right': '25px',
-          'padding-left': '25px',
+          'padding-right': '0px',
+          'padding-left': '0px',
         },
         stylable: [
           'padding', 'padding-top', 'padding-left', 'padding-right', 'padding-bottom',
