2871c5f1dcbb7e1d54bad99b36f70dba6c947c47
ivanalbizu <mariano-ivan.gonzalez-albizu@capgemini.com>
2022-12-27 08:08:54 +0100

    delete locales

--- locale/index.js
+++ locale/index.js
@@ -5,18 +5,6 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "ca", {
-  enumerable: true,
-  get: function get() {
-    return _ca.default;
-  }
-});
-Object.defineProperty(exports, "de", {
-  enumerable: true,
-  get: function get() {
-    return _de.default;
-  }
-});
 Object.defineProperty(exports, "en", {
   enumerable: true,
   get: function get() {
@@ -29,35 +17,7 @@ Object.defineProperty(exports, "es", {
     return _es.default;
   }
 });
-Object.defineProperty(exports, "fr", {
-  enumerable: true,
-  get: function get() {
-    return _fr.default;
-  }
-});
-Object.defineProperty(exports, "nl", {
-  enumerable: true,
-  get: function get() {
-    return _nl.default;
-  }
-});
-Object.defineProperty(exports, "pt", {
-  enumerable: true,
-  get: function get() {
-    return _pt.default;
-  }
-});
-
-var _ca = _interopRequireDefault(require("./ca"));
-
-var _de = _interopRequireDefault(require("./de"));
 
 var _en = _interopRequireDefault(require("./en"));
 
 var _es = _interopRequireDefault(require("./es"));
-
-var _fr = _interopRequireDefault(require("./fr"));
-
-var _nl = _interopRequireDefault(require("./nl"));
-
-var _pt = _interopRequireDefault(require("./pt"));
